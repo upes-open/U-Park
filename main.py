d778be11fca91ba47e8b56954e987f8567a684cc
Saarini Ritesh <saarini.ritesh@gmail.com>
2022-11-19 11:08:45 +0530

    database and email integration done

--- main.py
+++ main.py
@@ -8,7 +8,63 @@ from tkinter import ttk
 from tkinter import font
 from turtle import color
 from PIL import Image, ImageTk
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from email.message import EmailMessage
+import ssl
+import smtplib
 
+#establishing connection with google sheets and inserting license plate number to generate slot
+def googlesheet(plate_number):
+    try:
+        #estabilishing the connectivity between the google sheet
+        scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+        creds = ServiceAccountCredentials.from_json_keyfile_name("reading/gs_credentials.json",scope)
+        client = gspread.authorize(creds)
+        sheet = client.open("Parking slots").worksheet("Sheet1")
+        #The sheet holding the faculty information
+        #sheet2=  client.open("Parking slots").worksheet("Sheet2") #has details of the owner of vehicle
+    except Exception as e:
+        print("The connection was not estabilished.")
+        return 
+    cells=sheet.col_values(col=1)
+    s_no=len(cells)
+    insertrow=[plate_number,s_no]
+    sheet.insert_row(insertrow,s_no+1)
+    send(plate_number,s_no+1)
+
+#sending email with parking slot number
+def send(plate_number,slot):
+    #estabilishing the connectivity between the google sheet
+    scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("reading/gs_credentials.json",scope)
+    client = gspread.authorize(creds)
+    #The sheet holding the faculty information
+    sheet2=  client.open("Parking slots").worksheet("Sheet2") #has details of the owner of vehicle
+    #fetching the required email address
+    RecieverDetails = sheet2.row_values(sheet2.find(str(plate_number)).row)
+
+    #preparing the mail contents
+    sender = "automatedparkingopen@gmail.com" #email id of sender
+    password = "fgccvkxetqnqomps" #password for the sender's account
+    reciever = str(RecieverDetails[-1]) #email id of the receiver
+    subject = "This is your parking slot" #subject of the email
+    body = f"You have to park your vehical at slot number {str(slot)}" #body of the email
+
+    #collecting all the elments of the email message
+    e = EmailMessage() #making the message
+    e['From'] = sender #setting the from as the sender's email id
+    e['To'] = reciever #setting the to as the reciever's email id
+    e['Subject'] = subject #setting the subject in the email
+    e.set_content(body) #combining the contents
+
+    #creating the context
+    con = ssl.create_default_context()
+
+    #sending the email using the smtplib module
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=con) as smtp:
+        smtp.login(sender,password) #the account from which it is to be send
+        smtp.sendmail(sender,reciever,e.as_string()) #the from,to and content of the email to be sent ''')
 #function to detect and read license plate numbers
 def plate_read(a):
     img = a
@@ -121,7 +177,7 @@ def ReadFromVideo(p):
     cv2.destroyAllWindows()
 
 #Reading from video
-ReadFromVideo('VN20221021_095309.mp4')
+ReadFromVideo('dataset/N20221021_095309.mp4')
 
 #Reading from webcam
 #ReadFromVideo(0)
